5436996b321a254bd991e0c86362523616b24e7e
Ranveer225 <singr12@farmingdale.edu>
2024-10-25 20:47:54 -0400

    ToDo 1: The splash screen should have the image displayed in the center of the screen.

--- app/src/main/java/edu/farmingdale/pizzapartybottomnavbar/Screen3.kt
+++ app/src/main/java/edu/farmingdale/pizzapartybottomnavbar/Screen3.kt
@@ -8,7 +8,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Slider
@@ -27,7 +26,6 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 // ToDo 2: the slider should be able to change the text value of the screen
-
 // ToDo 3: Make the UI look better by adding a gradient background (vertical) and padding
 
 @Composable
@@ -35,27 +33,31 @@ fun Screen3() {
     var sliderValue by remember { mutableStateOf(0.5f) }
     var chkd by remember { mutableStateOf(true) }
 
-
     val context = LocalContext.current
-    Column ( modifier = Modifier.padding(horizontal = 20.dp).fillMaxSize(),
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(
+                Brush.verticalGradient(
+                    colors = listOf(Color.LightGray, Color.DarkGray)
+                )
+            )
+            .padding(horizontal = 20.dp),
         verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally){
-        Slider(value = sliderValue, onValueChange = { sliderValue=it }, Modifier.fillMaxWidth()
-            , enabled = chkd)
-
-        Text (fontSize = 20.sp, text = "Second Screen" )
-
-        Button(onClick = { val newInt = Intent(Intent.ACTION_VIEW)
-            newInt.setData(Uri.parse("tel:6314202000"))
-            context.startActivity(newInt) }) {
-            Text(fontSize = 20.sp, text ="Call me")
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Slider(value = sliderValue, onValueChange = { sliderValue = it }, Modifier.fillMaxWidth(), enabled = chkd)
+        Text(
+            fontSize = 20.sp,
+            text = if (sliderValue == 0.5f) "Second Screen" else "Slider Value: ${(sliderValue * 100).toInt()}%"
+        )
+        Button(onClick = {
+            val newInt = Intent(Intent.ACTION_VIEW)
+            newInt.data = Uri.parse("tel:6314202000")
+            context.startActivity(newInt)
+        }) {
+            Text(fontSize = 20.sp, text = "Call me")
         }
-
-        Checkbox(checked = chkd, onCheckedChange = { chkd=it }, modifier = Modifier.padding(10.dp))
-
+        Checkbox(checked = chkd, onCheckedChange = { chkd = it }, modifier = Modifier.padding(10.dp))
     }
-
 }
-
-
-
--- app/src/main/java/edu/farmingdale/pizzapartybottomnavbar/SplashScreen.kt
+++ app/src/main/java/edu/farmingdale/pizzapartybottomnavbar/SplashScreen.kt
@@ -5,10 +5,7 @@ import androidx.compose.animation.core.Animatable
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -19,31 +16,31 @@ import androidx.compose.ui.res.painterResource
 import androidx.navigation.NavHostController
 import kotlinx.coroutines.delay
 
-
 // ToDo 1: The splash screen should have the image displayed in the center of the screen.
 
 @Composable
 fun SplashScreen(navController: NavHostController) {
 
-    val scale= remember {
-        Animatable(0f, 1f)
+    val scale = remember {
+        Animatable(0f)
     }
     LaunchedEffect(key1 = true) {
         scale.animateTo(
-            targetValue = 0.5f,
-            animationSpec = tween(durationMillis = 1000,0, easing = {
+            targetValue = 1f,
+            animationSpec = tween(durationMillis = 1000, easing = {
                 OvershootInterpolator(2f).getInterpolation(it)
-            }
-            ))
+            })
+        )
         delay(3000)
         navController.navigate(BottomNavigationItems.PizzaScreen.route)
     }
-
-    Column (modifier = Modifier
-        .wrapContentSize(Alignment.Center)){
-        Image(painter = painterResource(id = R.drawable.fsclogo), contentDescription ="" )
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.fsclogo),
+            contentDescription = "FSC Logo",
+        )
     }
 }
-
-
-
